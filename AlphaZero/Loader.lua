37c71ec66bfc64dc6586cfc2a6dd2c84e38b3ce9
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-25 18:33:39 -0500

    Add files via upload

--- AlphaZero/Loader.lua
+++ AlphaZero/Loader.lua
@@ -1,1 +1,12 @@
+local Client = {
+    Gitub = "",
+    Games = {
+        
+    },
+}
 
+for PlaceId, GameName in next, Client.Games do
+    if game.PlaceId == PlaceId then
+        loadstring(game:HttpGetAsync(Client.Gitub .. GameName .. ".lua"))()
+    end
+end
