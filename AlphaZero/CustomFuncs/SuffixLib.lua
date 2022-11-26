fb1a880bab14b80e17fc5398aaa917d0d73bbe05
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-26 13:31:20 -0500

    Add files via upload

---
+++ AlphaZero/CustomFuncs/SuffixLib.lua
@@ -0,0 +1,55 @@
+local Suffixes = {
+    "K",
+    "M",
+    "B",
+    "t",
+    "q",
+    "Q",
+    "s",
+    "S",
+    "o",
+    "n",
+    "d",
+    "U",
+    "D",
+    "T",
+    "Qt",
+    "Qd",
+    "Sd",
+    "St",
+    "O",
+    "N",
+    "v",
+    "c",
+    "La",
+    "De",
+    "Me",
+    "Hi",
+    "DS",
+    "Ds",
+}
+local SuffixLib = {}; do
+    function SuffixLib:ConvertToSuffix(Number)
+        local Number = tonumber(Number)
+        if Number < 1000 then
+            return Number
+        end
+        local Suffix = Suffixes[math.floor(math.log(Number) / math.log(1000))]
+        return string.format("%.2f", Number / 1000 ^ math.floor(math.log(Number) / math.log(1000))) .. Suffix
+    end
+
+    function SuffixLib:ConvertToNumber(String)
+        local String = tostring(String)
+        local Suffix = string.match(String, "%a+")
+        local Number = string.gsub(String, "%a+", "")
+        if not Suffix then
+            return Number
+        end
+        for i = 1, #Suffixes do
+            if Suffixes[i] == Suffix then
+                return Number * 1000 ^ i
+            end
+        end
+    end
+end
+return SuffixLib
