b4ea7ddce082d918a9ddf9069cfbe12f9f0f2ff4
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-25 21:29:05 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/Network.lua
+++ AlphaZero/CustomFuncs/Network.lua
@@ -1,1 +1,36 @@
+local Network = {}; do
+    function Network:Send(Remote, ...)
+        local Args = {...}
 
+        local Success, Error = pcall(function()
+            Remote:FireServer(unpack(Args))
+        end)
+
+        if not Success then
+            Remote:FireServer()
+        end
+    end
+    function Network:Invoke(Remote, ...)
+        local Args = {...}
+
+        local Success, Error = pcall(function()
+            Remote:InvokeServer(unpack(Args))
+        end)
+
+        if not Success then
+            error(Error)
+        end
+    end
+    function Network:Receive(Remote, Callback)
+        Remote.OnClientEvent:Connect(Callback)
+    end
+    function Network:Notify(UILib, Title, Content, Duration)
+        UILib:Notify({
+            Title = Title,
+            Content = Content,
+            Duration = Duration,
+            Image = 4483362458,
+         })
+    end
+end
+return Network
