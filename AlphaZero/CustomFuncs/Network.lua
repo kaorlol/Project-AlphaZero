6a83e89b9bdd07c172db15588474c9136ec8caa8
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-26 15:14:39 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/Network.lua
+++ AlphaZero/CustomFuncs/Network.lua
@@ -1,4 +1,9 @@
 local NotificationLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sw1ndlerScripts/RobloxScripts/main/Notification%20Library/main.lua"))()
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 local Network = {}; do
     function Network:Send(Remote, ...)
         local Args = {...}
@@ -43,6 +48,25 @@ local Network = {}; do
             TrueText = "Yes",
             FalseText = "No"
         })
+    end
+    function Network:TweenTo(CFrame, Time)
+        local TweenInfo = TweenInfo.new(Time, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+        local Tween = TweenService:Create(HumanoidRootPart, TweenInfo, {CFrame = CFrame})
+        Tween:Play()
+        Tween.Completed:Wait()
+    end
+    function Network:MoveTo(CFrame)
+        local PathfindingService = game:GetService("PathfindingService")
+        local Path = PathfindingService:CreatePath()
+        Path:ComputeAsync(HumanoidRootPart.Position, CFrame.Position)
+        local Waypoints = Path:GetWaypoints()
+
+        for _, Waypoint in next, Waypoints do
+            Humanoid:MoveTo(Waypoint.Position)
+        end
+    end
+    function Network:TeleportTo(CFrame)
+        Character:PivotTo(CFrame)
     end
 end
 return Network
