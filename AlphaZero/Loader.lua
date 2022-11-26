e084d735feed2a9590b918f9b0f7545ec6f97f8f
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-25 19:08:26 -0500

    Update Loader.lua

--- AlphaZero/Loader.lua
+++ AlphaZero/Loader.lua
@@ -1,12 +1,29 @@
 local Client = {
-    Gitub = "",
+    Github = "https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/Supported%20Games/",
     Games = {
-        
+        ["8069117419"] = "Demon%20Soul%20Simulator";
     },
 }
 
-for PlaceId, GameName in next, Client.Games do
-    if game.PlaceId == PlaceId then
-        loadstring(game:HttpGetAsync(Client.Gitub .. GameName .. ".lua"))()
+function LoadScript()
+    local function GetGame()
+        for PlaceId, GameName in next, Client.Games do
+            if tostring(game.PlaceId) == PlaceId then
+                return GameName
+            end
+        end
+    end
+    local GameName = GetGame()
+    if GameName then
+        local Success, Error = pcall(function()
+            loadstring(game:HttpGet(string.format("%s%s%s", Client.Github, GameName, ".lua")))()
+        end)
+        if not Success then
+            warn(string.format('Failed to load script for game: "%s", Error: %s', string.gsub(GameName, "%%20", " "), Error))
+        end
+    else
+        warn("AlphaZero does not support this game.")
     end
 end
+
+LoadScript()
