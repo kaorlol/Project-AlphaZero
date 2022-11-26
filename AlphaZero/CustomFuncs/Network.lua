8738137bab8049a53dcbe7279a3396fe9d3e31b6
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-11-26 16:42:19 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/Network.lua
+++ AlphaZero/CustomFuncs/Network.lua
@@ -68,5 +68,51 @@ local Network = {}; do
     function Network:TeleportTo(CFrame)
         Character:PivotTo(CFrame)
     end
+    function Network:SendInvite(DiscordID)
+        local Settings = {
+			InviteCode = DiscordID
+		}
+		
+		local HttpService = game:GetService("HttpService")
+		local RequestFunction
+		
+		if syn and syn.request then
+			RequestFunction = syn.request
+		elseif request then
+			RequestFunction = request
+		elseif http and http.request then
+			RequestFunction = http.request
+		elseif http_request then
+			RequestFunction = http_request
+		end
+		
+		local DiscordApiUrl = "http://127.0.0.1:%s/rpc?v=1"
+		
+		if not RequestFunction then
+			return self:Notify("Error", "Failed to find a request function", 5)
+		end
+		
+		for i = 6453, 6464 do
+			local DiscordInviteRequest = function()
+				local DiscordRequest = RequestFunction({
+					Url = string.format(DiscordApiUrl, tostring(i)),
+					Method = "POST",
+					Body = HttpService:JSONEncode({
+						nonce = HttpService:GenerateGUID(false),
+						args = {
+							invite = {code = Settings.InviteCode},
+							code = Settings.InviteCode
+						},
+						cmd = "INVITE_BROWSER"
+					}),
+					Headers = {
+						["Origin"] = "https://discord.com",
+						["Content-Type"] = "application/json"
+					}
+				})
+			end
+			task.spawn(DiscordInviteRequest)
+		end
+    end
 end
 return Network
