e9f6d188d878609df3d4af576a1db2a40d65dae2
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-12-11 10:10:44 -0500

    Add files via upload

---
+++ AlphaZero/CustomFuncs/AllUtils.lua
@@ -0,0 +1,7 @@
+local Utils = {
+    Pathfinding = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/PathfindingLib.lua")))();
+    Network = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/Network.lua")))();
+    Suffix = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/SuffixLib.lua")))();
+    Table = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/TableLib.lua")))();
+};
+return Utils
