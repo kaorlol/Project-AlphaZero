0b5c3171e29989eef9b2affd6404bc5b94f7bd11
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-12-12 20:43:26 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/PathfindingLib.lua
+++ AlphaZero/CustomFuncs/PathfindingLib.lua
@@ -13,14 +13,20 @@ LocalPlayer.CharacterAdded:Connect(function(Char)
 	HumanoidRootPart = Char:WaitForChild("HumanoidRootPart")
 end)
 
+function WorldToPoint(Position)
+    local Vector,_ = Camera:WorldToViewportPoint(Position)
+    local NewVector = Vector2.new(Vector.X, Vector.Y)
+    return NewVector
+end
+
 local Pathfinding = {}; do
     function Pathfinding:MoveTo(Position, Wait)
         local Start;
 
         if Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-            Start = Character.UpperTorso
-        else
-            Start = Character.Torso
+            Start = Character.UpperTorso or Character.Torso
+        elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+            Start = HumanoidRootPart
         end
 
         local Path = PathfindingService:FindPathAsync(Start.Position, Position);
@@ -34,13 +40,13 @@ local Pathfinding = {}; do
             if Waypoints[Waypoint].Action == Enum.PathWaypointAction.Jump then
                 Humanoid.Jump = true
                 Humanoid:MoveTo(Waypoints[Waypoint + 1].Position)
-                
+
                 if Wait then
                     Humanoid.MoveToFinished:Wait()
                 end
             else
                 Humanoid:MoveTo(Waypoints[Waypoint].Position)
-                
+
                 if Wait then
                     Humanoid.MoveToFinished:Wait()
                 end
@@ -52,9 +58,9 @@ local Pathfinding = {}; do
         local Start;
 
         if Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-            Start = Character.UpperTorso
-        else
-            Start = Character.Torso
+            Start = Character.UpperTorso or Character.Torso
+        elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+            Start = HumanoidRootPart
         end
 
         local Path = PathfindingService:FindPathAsync(Start.Position, Positions);
@@ -66,7 +72,7 @@ local Pathfinding = {}; do
 
         for Waypoint = 1, #Waypoints do
             local TweenInfo = TweenInfo.new(0, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
-            local Tween = TweenService:Create(HumanoidRootPart, TweenInfo, {CFrame = CFrame.new(Waypoints[Waypoint].Position)})
+            local Tween = TweenService:Create(HumanoidRootPart, TweenInfo, {CFrame = CFrame.new(Waypoints[Waypoint + 5].Position)})
             Tween:Play()
 
             if Wait then
@@ -75,25 +81,69 @@ local Pathfinding = {}; do
         end
     end;
 
-    function Pathfinding:DrawPath(Positions)
-        local Start;
+    function Pathfinding:DrawPath(Position, Toggle)
+        local LoopToggle = Toggle;
+        if Toggle then
+            local Start;
 
-        if Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-            Start = Character.UpperTorso
-        else
-            Start = Character.Torso
-        end
+            if Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
+                Start = Character.UpperTorso or Character.Torso
+            elseif Character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+                Start = HumanoidRootPart
+            end
 
-        local Path = PathfindingService:FindPathAsync(Start.Position, Positions);
-        local Waypoints = Path:GetWaypoints();
+            local Path = PathfindingService:FindPathAsync(Start.Position, Position);
+            local Waypoints = Path:GetWaypoints();
 
-        for Waypoint = 1, #Waypoints do
-            local Part = Instance.new("Part")
-            Part.Anchored = true
-            Part.CanCollide = false
-            Part.Size = Vector3.new(1, 1, 1)
-            Part.Position = Waypoints[Waypoint].Position
-            Part.Parent = workspace
+            if #Waypoints == 0 then
+                print("No path found")
+                return;
+            end
+
+            local Lines = {}
+
+            for Waypoint = 1, #Waypoints do
+                local Line = Drawing.new("Line");
+
+                Line.Visible = true;
+                Line.From = WorldToPoint(Waypoints[Waypoint].Position);
+
+                local LineTo;
+                if Waypoints[Waypoint + 1] then
+                    LineTo = Waypoints[Waypoint + 1].Position;
+                else
+                    LineTo = Position;
+                end
+
+                Line.To = WorldToPoint(LineTo);
+
+                Line.Color = Color3.fromRGB(255, 255, 255);
+                Line.Thickness = 2;
+                Line.Transparency = 1;
+
+                table.insert(Lines, {
+                    Line = Line,
+                    To = LineTo,
+                    From = Waypoints[Waypoint].Position
+                });
+            end;
+
+            task.spawn(function()
+                while LoopToggle do task.wait()
+                    for _, Line in next, Lines do
+                        local _, OnScreen = Camera:WorldToViewportPoint(Line.From)
+
+                        if OnScreen then
+                            Line.Line.Visible = true
+                        else
+                            Line.Line.Visible = false
+                        end
+
+                        Line.Line.From = WorldToPoint(Line.From)
+                        Line.Line.To = WorldToPoint(Line.To)
+                    end
+                end
+            end)
         end
     end;
 end
