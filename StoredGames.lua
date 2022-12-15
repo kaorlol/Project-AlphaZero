95248e6e2596d97a22c877c55d4d0ac13eee0a6c
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-12-15 18:24:06 -0500

    Delete StoredGames.lua

--- StoredGames.lua
+++
@@ -1,7 +0,0 @@
-return {
-    ["8069117419"] = "Demon%20Soul%20Simulator";
-    ["10898965735"] = "Mine%20Racer";
-    ["7525610732"] = "Kaizen";
-    ["6569830174"] = "Lemon%20Funky";
-    ["6679968919"] = "Fly%20Race";
-}
