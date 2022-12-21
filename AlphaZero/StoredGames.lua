5aa62600f2ba65bc1f90c99b202fa05950eb05b2
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2022-12-21 13:37:33 -0800

    Update StoredGames.lua

--- AlphaZero/StoredGames.lua
+++ AlphaZero/StoredGames.lua
@@ -6,5 +6,5 @@ return {
     ["6679968919"] = "Fly%20Race";
     ["1537690962"] = "Bee%20Swarm%20Simulator";
     ["7305309231"] = "Taxi%20Boss";
-    ['2443177453'] = "Basically%20FNF";
+    ['6520999642'] = "Basically%20FNF";
 }
