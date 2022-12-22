e7548da14c87677a91d01912c43713982490db6c
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-12-21 22:21:47 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/TableLib.lua
+++ AlphaZero/CustomFuncs/TableLib.lua
@@ -1,28 +1,25 @@
 local LuaEncode = loadstring(game:HttpGet(("https://raw.githubusercontent.com/regginator/LuaEncode/master/src/LuaEncode.lua")))();
 
-local function PrintTable(Table)
-    return print(LuaEncode(Table, {
-        PrettyPrinting = true;
-        IndentCount = 4;
-    }))
-end
+local TableLib = {}; do
+    function TableLib:PrintTable(Table)
+        return print(LuaEncode(Table, {
+            PrettyPrinting = true;
+            IndentCount = 4;
+        }))
+    end;
 
-local function TableToString(Table)
-    return LuaEncode(Table, {
-        PrettyPrinting = true;
-        IndentCount = 4;
-    })
-end
+    function TableLib:TableToString(Table)
+        return LuaEncode(Table, {
+            PrettyPrinting = true;
+            IndentCount = 4;
+        })
+    end;
 
-local function CopyTable(Table)
-    return LuaEncode(Table, {
-        PrettyPrinting = true;
-        IndentCount = 4;
-    })
-end
-
-return {
-    PrintTable = PrintTable;
-    TableToString = TableToString;
-    CopyTable = CopyTable;
-};
+    function TableLib:CopyTable(Table)
+        return LuaEncode(Table, {
+            PrettyPrinting = true;
+            IndentCount = 4;
+        })
+    end;
+end;
+return TableLib;
