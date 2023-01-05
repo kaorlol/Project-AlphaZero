843e6dd7ab2d41e865fbeaf2dba3c37f1d51bf8f
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-01-04 20:02:45 -0500

    Add files via upload

--- AlphaZero/CustomFuncs/EntityLib.lua
+++ AlphaZero/CustomFuncs/EntityLib.lua
@@ -1,5 +1,7 @@
 local Request = (syn and syn.request) or request or http_Request or (http and http.request)
 local Entity = nil;
+local PathfindingService = game:GetService("PathfindingService");
+local TweenService = game:GetService("TweenService");
 
 local EntityLib = {}; do
     function EntityLib:Require(Url)
@@ -60,8 +62,62 @@ local EntityLib = {}; do
     function EntityLib:TeleportTo(Position)
         Entity.character.HumanoidRootPart.CFrame = CFrame.new(Position)
     end
+
+    function EntityLib:MoveTo(Position, Wait)
+        local Start;
+
+        if Entity.character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
+            Start = Entity.character.UpperTorso or Entity.character.Torso;
+        elseif Entity.character.Humanoid.RigType == Enum.HumanoidRigType.R6 then
+            Start = Entity.character.HumanoidRootPart;
+        end
+
+        local Path = PathfindingService:FindPathAsync(Start.Position, Position);
+        local Waypoints = Path:GetWaypoints();
+
+        if #Waypoints == 0 then
+            self:TeleportTo(Position);
+        end
+
+        for Waypoint = 1, #Waypoints do
+            if Waypoints[Waypoint].Action == Enum.PathWaypointAction.Jump then
+                Entity.character.Humanoid.Jump = true;
+                Entity.character.Humanoid:MoveTo(Waypoints[Waypoint + 1].Position)
+
+                if Wait then
+                    Entity.character.Humanoid.MoveToFinished:Wait();
+                end
+            else
+                Entity.character.Humanoid:MoveTo(Waypoints[Waypoint].Position);
+
+                if Wait then
+                    Entity.character.Humanoid.MoveToFinished:Wait();
+                end
+            end
+        end
+    end;
+
+    function EntityLib:TweenTo(Position, Time)
+        local Tween = TweenService:Create(Entity.character.HumanoidRootPart, TweenInfo.new(Time, Enum.EasingStyle.Linear), {
+            CFrame = CFrame.new(Position);
+        })
+        local StabilizerTween = TweenService:Create(Entity.character.HumanoidRootPart, TweenInfo.new(0.25, Enum.EasingStyle.Linear), {
+            CFrame = CFrame.new(Position);
+        })
+
+        Tween:Play();
+        Tween.Completed:Connect(function()
+            Entity.character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0);
+            StabilizerTween:Play();
+        end)
+
+        StabilizerTween.Completed:Connect(function()
+            Entity.character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0);
+        end)
+    end
 end
 
-Entity = EntityLib:Run(EntityLib:Require("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua", true, true))
-Entity.fullEntityRefresh()
-return Entity, EntityLib
+Entity = EntityLib:Run(EntityLib:Require("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/Libraries/entityHandler.lua", true, true));
+Entity.fullEntityRefresh();
+
+return Entity, EntityLib;
