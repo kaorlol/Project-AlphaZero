0f5670706b9f04c5272ef5acb3e4c558bb30b623
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2023-02-26 18:07:57 -0800

    Update AllUtils.lua

--- AlphaZero/CustomFuncs/AllUtils.lua
+++ AlphaZero/CustomFuncs/AllUtils.lua
@@ -1,8 +1,9 @@
-local Utils = {
-    Pathfinding = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/PathfindingLib.lua")))();
-    Network = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/Network.lua")))();
-    Webhook = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/WebhookLib.lua")))();
-    Suffix = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/SuffixLib.lua")))();
-    Table = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/TableLib.lua")))();
-};
-return Utils
+local u_tils = {
+    Pathfinding = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/PathfindingLib.lua"))(),
+    Network = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/Network.lua"))(),
+    Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/WebhookLib.lua"))(),
+    Suffix = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/SuffixLib.lua"))(),
+    Table = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/TableLib.lua"))(),
+    Esp = loadstring(game:HttpGet("https://github.com/Uvxtq/Project-AlphaZero/blob/main/AlphaZero/CustomFuncs/EntityLib.lua?raw=true"))()
+}
+return u_tils
