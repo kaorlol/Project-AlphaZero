d4961185c5a9d4d790c650a7e855e9f0685e57a7
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2023-02-26 18:15:20 -0800

    Update AllUtils.lua

--- AlphaZero/CustomFuncs/AllUtils.lua
+++ AlphaZero/CustomFuncs/AllUtils.lua
@@ -4,6 +4,6 @@ local u_tils = {
     Webhook = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/WebhookLib.lua"))(),
     Suffix = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/SuffixLib.lua"))(),
     Table = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/TableLib.lua"))(),
-    Esp = loadstring(game:HttpGet("https://github.com/Uvxtq/Project-AlphaZero/blob/main/AlphaZero/CustomFuncs/EntityLib.lua?raw=true"))()
+    Esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/CustomFuncs/EspLib.lua"))()
 }
 return u_tils
