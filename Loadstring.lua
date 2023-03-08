7a84740ebdb5ec1078d617ffb277067b19bb9bc8
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2023-03-07 22:00:24 -0500

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/Loader.lua")))();
+--loadstring(game:HttpGet(("https://raw.githubusercontent.com/Uvxtq/Project-AlphaZero/main/AlphaZero/Loader.lua")))();
+loadstring(game:HttpGet('https://scripts.luawl.com/17509/AlphaZero.lua'))()
